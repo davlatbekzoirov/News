e69a8b7db5a1f44b208b70a6b0dd2a862ba1f80f
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-23 02:49:25 +0500

    created models Category and News + media directory

--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class News(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    image = models.ImageField(upload_to='news/images', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    publish_time = models.DateTimeField(default=timezone.now)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish_time']
+
+    def __str__(self):
+        return self.title
