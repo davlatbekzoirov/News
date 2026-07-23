2d002d2d95e5afe6ce9906bf24e5362244582dd8
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-23 17:50:28 +0500

    added templates

--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, News
 
-# Create your views here.
+def news_list(request):
+    news_list = News.published.all()
+    context = {
+        'news_list': news_list,
+    }
+    return render(request, "news/news_list.html", context=context)
+
+def news_detail(request, id):
+    news = get_object_or_404(News, pk=id, status=News.Status.PUBLISHED)
+    context = {
+        'news': news,
+    }
+
+    return render(request, "news/news_detail.html", context=context)
