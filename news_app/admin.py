d54fd01bd7fc7f19cd044a7c5b8d128d013eff1b
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-24 16:04:10 +0500

    added Contact to admin.py

--- news_app/admin.py
+++ news_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import News, Category
+from .models import News, Category, Contact
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
@@ -14,3 +14,5 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Contact)
