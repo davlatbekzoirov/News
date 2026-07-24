698e46d58f1834f1e126b59f3b2eb60435db6806
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-24 16:47:26 +0500

    update urls.py

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import news_list, news_detail, homePageView, contactPageView, categoryPageView
+from .views import news_list, news_detail, homePageView, ContactPageView, categoryPageView
 
 urlpatterns = [
     path('', homePageView, name='home_page'),
-    path('contact-us/', contactPageView, name='contact_page'),
+    path('contact-us/', ContactPageView.as_view(), name='contact_page'),
     path('category/', categoryPageView, name='category_page'),
     path('news/', news_list, name='all_news_list'),
     path('news/<int:id>/', news_detail, name='news_detail_page'),
