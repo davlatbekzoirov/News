e42b1786da4bd3e50d70ac629822af44d6568a8f
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-27 00:42:05 +0500

    update urls

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import news_list, news_detail, homePageView, ContactPageView, categoryPageView, HomePageView
+from .views import news_list, news_detail, homePageView, ContactPageView, categoryPageView, HomePageView, \
+    LocalNewsView, SportNewsView, AbroadNewsView, TechnologyNewsView, AvtoNewsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
@@ -7,4 +8,9 @@ urlpatterns = [
     path('category/', categoryPageView, name='category_page'),
     path('news/', news_list, name='all_news_list'),
     path('news/<slug:news>/', news_detail, name='news_detail_page'),
+    path('local-news/', LocalNewsView.as_view(), name='local_news_page'),
+    path('sport-news/', SportNewsView.as_view(), name='sport_news_page'),
+    path('abroad-news/', AbroadNewsView.as_view(), name='abroad_news_page'),
+    path('technology-news/', TechnologyNewsView.as_view(), name='technology_news_page'),
+    path('avto-news/', AvtoNewsView.as_view(), name='avto_news_page'),
 ]
