1da4ac82277a0234f1c98c477c16901fcaf0c4d9
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-27 18:34:04 +0500

    created login views, page, and forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('You are logged in')
+                else:
+                    return HttpResponse('You are not active')
+            else:
+                return HttpResponse('Invalid login details')
+    else:
+        form = LoginForm()
+        context = {'form': form}
+    return render(request, 'account/login.html', context=context)
