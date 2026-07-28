d78339baf55fa248df147ad38e994380bdacf40f
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-28 17:26:37 +0500

    update urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import user_login, dashboard_view
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, \
+    PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView
 
 urlpatterns = [
     # path('login/', user_login, name='login'),
@@ -8,5 +9,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
     path('password-change/', PasswordChangeView.as_view(), name='password_change'),
     path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('profile/', dashboard_view, name='user_profile'),
 ]
