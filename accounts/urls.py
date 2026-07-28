46f037fa8bbedc8f1c36780e698499a6346d0c07
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-28 17:27:13 +0500

    update urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('password-change/', PasswordChangeView.as_view(), name='password_change'),
     path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('profile/', dashboard_view, name='user_profile'),
 ]
