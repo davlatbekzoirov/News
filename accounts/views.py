82d32d6377f4fac81e4a284cf5cc69f972acea1e
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-03 21:25:26 +0500

    create register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
+from multiprocessing import context
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 def user_login(request):
     if request.method == 'POST':
@@ -29,3 +31,17 @@ def dashboard_view(request):
     }
 
     return render(request, 'pages/user_profile.html', context)
+
+def user_register(request):
+    if request.method == 'POST':
+        user_form = RegisterForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            context = {'new_user': user_form}
+            return render(request, 'account/register_done.html', context=context)
+    else:
+        user_form = RegisterForm()
+        context = {'user_form': user_form}
+    return render(request, 'account/register.html', context=context)
