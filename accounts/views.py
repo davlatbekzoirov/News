1c28bcb8514398fd221dc074532f7012b4213d87
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-03 21:41:46 +0500

    create register class

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
-from multiprocessing import context
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 from .forms import LoginForm, RegisterForm
+from django.views.generic import CreateView
 
 def user_login(request):
     if request.method == 'POST':
@@ -46,3 +48,8 @@ def user_register(request):
         user_form = RegisterForm()
     context = {'user_form': user_form}
     return render(request, 'account/register.html', context=context)
+
+class SignUp(CreateView):
+    form_class = RegisterForm
+    success_url = reverse_lazy('login')
+    template_name = 'account/register.html'
